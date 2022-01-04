72d79b3272ef719ac00c9a4a5ba951e80978026e
wonderfan <fanjiahe2000@163.com>
2022-01-04 07:53:02 +0000

    feat: open api

---
+++ open-api.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# use open-api code generator docker image to generate codes
+
+# use file command and sed command to refine codes
+
+# use golang docker image to make build and verify codes.
