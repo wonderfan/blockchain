77db127b795e440e84e6743ce12368505685fcc0
wonderfan <fanjiahe2000@163.com>
2022-02-17 12:39:58 +0000

    os: commands

--- darwin.sh
+++ darwin.sh
@@ -7,3 +7,13 @@ otool -l app
 otool -L binary-package
 
 install_name_tool -rpath old new
+
+echo $MY_LIB_PATH
+
+sudo find / -name 'libjvm.dylib'
+
+install_name_tool -add_rpath $libpath cosmovisor
+
+otool -l cosmovisor
+
+
