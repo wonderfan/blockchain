ad0ea357e40fb3fc6e8d6ccae43f4cbccf74777d
Afanti <fanjiahe2000@163.com>
2022-12-07 07:59:58 +0800

    feat: gaia version

--- test/cosmos/gaia.sh
+++ test/cosmos/gaia.sh
@@ -1,2 +1,11 @@
 #!/usr/bin/env bash
 
+# v6.0.0 : cosmos-sdk v0.44.3, tendermint v0.34.14
+# https://github.com/cosmos/gaia/blob/v6.0.0/go.mod
+
+# v5.0.8 : cosmos-sdk v0.42.10, tendermint v0.34.13
+
+git clone -b v5.0.8 https://github.com/cosmos/gaia.git
+
+# 
+
